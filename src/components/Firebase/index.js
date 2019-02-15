5644f036bd06fd956ba14382cc2b0caeb1b5b8b8
Stein Setvik <stein@activelylearn.com>
2019-02-15 10:30:42 -0800

    Completed > Provide Firebase in React

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
+import FirebaseContext from './context';
+import Firebase from './firebase';
 
-const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
-);
+export default Firebase;
 
-export default App;
+export { FirebaseContext };
